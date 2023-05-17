13fccfb069fd4119757ba051d0bebae97d454d88
Bruk Tedla <biruk.tedla93@gmail.com>
2023-05-17 15:10:23 +0300

    feat(backend-BrukTedla): add BaseDto

---
+++ TaskManagementSystem.Application/DTOs/Common/BaseDto.cs
@@ -0,0 +1,8 @@
+namespace TaskManagementSystem.Application.DTOs.Common;
+
+public abstract class BaseDto
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
