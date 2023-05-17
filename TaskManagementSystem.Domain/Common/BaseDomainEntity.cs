107a7f373dcb42eb1923aad85a3330505eea0d2b
Bruk Tedla <biruk.tedla93@gmail.com>
2023-05-17 14:46:00 +0300

    feat(backend-BrukTedla): add base domain entity

---
+++ TaskManagementSystem.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,8 @@
+namespace TaskManagementSystem.Domain.Common;
+
+public abstract class BaseDomainEntity
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
