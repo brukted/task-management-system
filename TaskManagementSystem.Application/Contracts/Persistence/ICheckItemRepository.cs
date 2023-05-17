80abe00ebbd70e3aae01ad63dd83407e31ff9c46
Bruk Tedla <biruk.tedla93@gmail.com>
2023-05-17 15:01:41 +0300

    feat(backend-BrukTedla): add ICheckItemRepository

---
+++ TaskManagementSystem.Application/Contracts/Persistence/ICheckItemRepository.cs
@@ -0,0 +1,7 @@
+using TaskManagementSystem.Domain;
+
+namespace TaskManagementSystem.Application.Contracts.Persistence;
+
+public interface ICheckItemRepository : IGenericRepository<CheckItem>
+{
+}
