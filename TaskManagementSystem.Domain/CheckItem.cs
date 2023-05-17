88b8c97a5834911f964d856b8fbf7e0779703ae9
Bruk Tedla <biruk.tedla93@gmail.com>
2023-05-17 14:49:42 +0300

    feat(backend-BrukTedla): add check item domain entity

---
+++ TaskManagementSystem.Domain/CheckItem.cs
@@ -0,0 +1,12 @@
+using TaskManagementSystem.Domain.Common;
+
+namespace TaskManagementSystem.Domain;
+
+public class CheckItem : BaseDomainEntity
+{
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public int UserId { get; set; }
+    public int TaskId { get; set; }
+    public string Status { get; set; } = null!;
+}
