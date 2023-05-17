65157a8037ba929fceabbc716eb0e639fa9946cd
Bruk Tedla <biruk.tedla93@gmail.com>
2023-05-17 15:15:55 +0300

    feat(backend-BrukTedla): add TaskDto

---
+++ TaskManagementSystem.Application/DTOs/Task/TaskDto.cs
@@ -0,0 +1,13 @@
+using TaskManagementSystem.Application.DTOs.Common;
+
+namespace TaskManagementSystem.Application.DTOs.Task;
+
+public class TaskDto : BaseDto, ITaskDto
+{
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public int UserId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Status { get; set; } = null!;
+}
