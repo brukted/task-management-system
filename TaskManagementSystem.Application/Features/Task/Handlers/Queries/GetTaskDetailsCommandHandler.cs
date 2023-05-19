25133f5511f31f2d3a078cd93f7a9febe578d3b1
Bruk Tedla <biruk.tedla93@gmail.com>
2023-05-19 14:41:49 +0300

    feat(backend-BrukTedla): GetTaskDetailsCommandHandler

---
+++ TaskManagementSystem.Application/Features/Task/Handlers/Queries/GetTaskDetailsCommandHandler.cs
@@ -0,0 +1,33 @@
+using AutoMapper;
+using MediatR;
+using TaskManagementSystem.Application.Contracts.Persistence;
+using TaskManagementSystem.Application.DTOs.Task;
+using TaskManagementSystem.Application.Exceptions;
+using TaskManagementSystem.Application.Features.Task.Requests.Queries;
+
+namespace TaskManagementSystem.Application.Features.Task.Handlers.Queries;
+
+public class GetTaskDetailsCommandHandler : IRequestHandler<GetTaskDetailsCommand, TaskDto>
+{
+    private readonly ITaskRepository _taskRepository;
+    private readonly Mapper _mapper;
+
+    public GetTaskDetailsCommandHandler(ITaskRepository taskRepository, Mapper mapper)
+    {
+        _taskRepository = taskRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<TaskDto> Handle(GetTaskDetailsCommand request, CancellationToken cancellationToken)
+    {
+        var task = await _taskRepository.Get(request.Id);
+
+        if (task == null)
+        {
+            // Handle the case where the task with the specified ID doesn't exist
+            throw new NotFoundException(nameof(Domain.Task), request.Id);
+        }
+
+        return _mapper.Map<TaskDto>(task);
+    }
+}
